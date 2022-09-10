77bf76d51aad711b7c0001fe02afe40097bcd1c4
Gabriel Rabelo <contato.ideialisa@gmail.com>
2022-09-10 09:56:55 -0300

    integração com api

--- src/pages/matchs.js
+++ src/pages/matchs.js
@@ -1,8 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Layout, Card } from "../components";
+import api from "../services/api";
 
 export default function MatchPage() {
   const [index, setIndex] = useState(0);
+  const [likes, setLikes] = useState([]);
+  const [matches, setMatches] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const [requestMatches, requestLikes] = await Promise.all([
+        api.get("/interactions/matches"),
+        api.get("/interactions/liked"),
+      ]);
+
+      if (requestLikes.status === 200) {
+        setLikes(requestLikes.data.data);
+      }
+
+      if (requestMatches.status === 200) {
+        setMatches(requestMatches.data.data);
+      }
+    }
+    getData();
+  }, []);
 
   const Tabs = () => {
     const handleTab = (tab) => {
@@ -28,8 +49,8 @@ export default function MatchPage() {
     );
   };
 
-  const likes = [];
-  const matches = [];
+  // const likes = [];
+  // const matches = [];
 
   const TabPanel = ({ children, index: selfIndex }) => {
     return selfIndex === index ? (
@@ -50,11 +71,11 @@ export default function MatchPage() {
         </ul>
       </TabPanel>
       <TabPanel index={1}>
-        <ul>
+        {/* <ul>
           {matches.map((record, key) => (
             <Card profile={record} key={key} variant="small" />
           ))}
-        </ul>
+        </ul> */}
       </TabPanel>
     </Layout>
   );
